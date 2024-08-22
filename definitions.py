4e1871d2dc7c3acde3db24fc86824cd52e770ec2
Yuhan Luo <4531914+yuhan@users.noreply.github.com>
2024-08-21 20:46:01 -0700

    PipesContext and PipesMessages

--- definitions.py
+++ definitions.py
@@ -1,41 +1,37 @@
-import shutil
-
 from dagster import (
-    AssetCheckExecutionContext,
-    AssetCheckResult,
+    AssetExecutionContext,
     Definitions,
     PipesSubprocessClient,
-    AssetExecutionContext,
     asset,
-    file_relative_path,
 )
 
 # here the asset is generated in-line
 # but you could also generate via asset factories
 
+
 @asset(
     # add other useful metadata
     compute_kind="typescript",
     owners=["elt_team@company.com"],
-    tags={
-        "dagster/storage_kind": "snowflake",
-        "typescript_file": "helloWorld.ts"
-    },
+    tags={"dagster/storage_kind": "snowflake", "typescript_file": "helloWorld.ts"},
 )
 def my_asset(
-    context: AssetExecutionContext, 
+    context: AssetExecutionContext,
     pipes_subprocess_client: PipesSubprocessClient,
 ):
-    """ Runs our ELT pipeline written in typescript"""
+    """Runs our ELT pipeline written in typescript"""
     cmd = [
-        shutil.which("node"),
-        file_relative_path(__file__, "dist/helloWorld.js"),
+        "npx",
+        "ts-node",
+        "src/helloWorld.ts",
     ]
 
     context.log.info(f"Will run command {cmd}")
 
     return pipes_subprocess_client.run(
-        command=cmd, context=context.op_execution_context
+        command=cmd,
+        context=context.op_execution_context,
+        extras={"total_count": 10},
     ).get_materialize_result()
 
 
--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -1,4 +1,83 @@
 // helloWorld.ts
-const message: string = "Hello, World!";
-console.log(message);
 
+import * as fs from "fs";
+import * as zlib from "zlib";
+
+function _decodeParam(value: string): any {
+  // Decode from base64
+  const decoded = Buffer.from(value, "base64");
+  // Decompress with zlib
+  const decompressed = zlib.inflateSync(decoded);
+  // Deserialize from JSON
+  return JSON.parse(decompressed.toString("utf-8"));
+}
+
+// Accessing a specific environment variable
+function _getEnvVar(key: string): string {
+  const myEnvVar = process.env[key];
+  if (myEnvVar) {
+    return myEnvVar;
+  } else {
+    return "";
+  }
+}
+
+function getPipesContext(): any {
+  // first, get the env var value for where the pipes context is stored
+  const encodedPipesContextParam = _getEnvVar("DAGSTER_PIPES_CONTEXT");
+  // then, decode the base64 encoded string
+  const decodedPipesContextParam = _decodeParam(encodedPipesContextParam);
+  const path = decodedPipesContextParam.path;
+  const pipesContext = JSON.parse(fs.readFileSync(path, "utf-8"));
+  return pipesContext;
+}
+
+function setPipesMessages(message: any) {
+  // TODO: this can be better - wrap in a class so de/encode is done in the constructor
+
+  // first, get the env var value for where the pipes message is stored
+  const encodedPipesMessagesParam = _getEnvVar("DAGSTER_PIPES_MESSAGES");
+  // then, decode the base64 encoded string
+  const decodedPipesMessagesParam = _decodeParam(encodedPipesMessagesParam);
+  const path = decodedPipesMessagesParam.path;
+
+  fs.appendFileSync(path, JSON.stringify(message) + "\n");
+}
+
+async function main() {
+  const pipesContext = getPipesContext();
+
+  // HIGHLIGHT 1: get the asset key from the pipes context
+  const asset_key = pipesContext.asset_keys[0];
+  const message = `Hello, ${asset_key}!`;
+
+  // HIGHLIGHT 2: this will be logged to stdout and
+  // captured by dagster (you can find it in UI: run logs > stdout)
+  console.log(message);
+
+  async function runCountdown(count: number) {
+    if (count < 1) return;
+
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        // HIGHLIGHT 3: report the countdown back to dagster
+        setPipesMessages({
+          method: "log",
+          params: { message: `Countdown Timer: ${count}` },
+        });
+        resolve();
+      }, 1000);
+    });
+
+    // Recursively call runCountdown for the next number
+    await runCountdown(count - 1);
+  }
+
+  // get the total count from the pipes context, passed as an extra
+  const totalCount = pipesContext.extras["total_count"];
+  await runCountdown(totalCount);
+
+  setPipesMessages({ method: "log", params: { message: "Yay!" } });
+}
+
+main();
